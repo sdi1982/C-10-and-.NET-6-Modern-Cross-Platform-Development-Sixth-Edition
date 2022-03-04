870bbc84e0be7fa6ad8c336b91ae27ed1e69f3b7
Kenji Yamaucni <yamakenji@gmail.com>
2022-03-04 14:33:09 +0900

    Done: Revealing the extent of the C# vocabulary

--- Chapter02/Vocabulary/Program.cs
+++ Chapter02/Vocabulary/Program.cs
@@ -1,4 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // # error version
 
-Console.WriteLine("Hello, World!");
+using System.Reflection;
+
+System.Data.DataSet ds;
+HttpClient client;
+
+Assembly? assembly = Assembly.GetEntryAssembly();
+if (assembly == null) return;
+
+foreach (AssemblyName name in assembly.GetReferencedAssemblies())
+{
+  Assembly a = Assembly.Load(name);
+  int methodCount = 0;
+
+  foreach (TypeInfo t in a.DefinedTypes)
+  {
+    methodCount += t.GetMethods().Count();
+  }
+
+  Console.WriteLine(
+    "{0:N0} types with {1:N0} methods in {2} assembly.",
+    arg0: a.DefinedTypes.Count(),
+    arg1: methodCount, arg2: name.Name);
+}
