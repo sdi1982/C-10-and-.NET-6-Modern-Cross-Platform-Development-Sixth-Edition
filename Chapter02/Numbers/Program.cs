4842849e53b08b63399379b91d2dd759f4a979d2
Kenji Yamaucni <yamakenji@gmail.com>
2022-03-08 12:34:36 +0900

    Storing Numbers

---
+++ Chapter02/Numbers/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+uint naturalNumber = 23;
+int integerNumber = -23;
+float realNumber = 2.3F;
+double anotherRealNumber = 2.3;
+
+int decimalNotation = 2_000_000;
+int binaryNotation = 0b_0001_1110_1000_0100_1000_0000;
+int hexadecimalNotation = 0x_001E_8480;
+
+Console.WriteLine($"{decimalNotation == binaryNotation}");
+Console.WriteLine($"{decimalNotation == hexadecimalNotation}");
