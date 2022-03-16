899383b552e34029c1019ef5458ad0cbaa6336ad
yamakenji <yamakenji@gmail.com>
2022-03-16 19:03:55 +0900

    Exploring bitwise and binary shift operators

---
+++ Chapter03/BitwiseAndShiftOperators/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using static System.Console;
+
+int a = 10;
+int b = 6;
+
+WriteLine($"a = {a}");
+WriteLine($"b = {b}");
+WriteLine($"a & b = {a & b}");
+WriteLine($"a | b = {a | b}");
+WriteLine($"a ^ b = {a ^ b}");
+
