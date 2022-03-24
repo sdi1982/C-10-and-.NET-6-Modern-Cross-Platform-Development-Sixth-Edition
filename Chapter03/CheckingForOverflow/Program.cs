6ddb25d0eacbad26fc1939b8bf3072a90ade865e
yamakenji <yamakenji@gmail.com>
2022-03-24 18:18:58 +0900

    Wrapping error-prone code in a try block Throwing overflow exceptions with the checked statement

---
+++ Chapter03/CheckingForOverflow/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// Throwing overflow exceptions with the checked statement
+using static System.Console;
+
+try
+{
+  checked
+  {
+    int x = int.MaxValue - 1;
+    WriteLine($"Initial value: {x}");
+    x++;
+    WriteLine($"After incrementing: {x}");
+    x++;
+    WriteLine($"After incrementing: {x}");
+    x++;
+    WriteLine($"After incrementing: {x}");
+  }
+
+}
+catch (OverflowException)
+{
+  WriteLine("The code overflowed but I caught the exception.");
+}
+
+unchecked
+{
+  int y = int.MaxValue + 1;
+  WriteLine($"Initial value: {y}");
+  y--;
+  WriteLine($"After decrementing: {y}");
+  y--;
+  WriteLine($"After decrementing: {y}");
+}
---
+++ Chapter03/HandlingExceptions/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using static System.Console;
+
+WriteLine("Before parsing");
+Write("What is your age? ");
+
+string? input = ReadLine();
+try
+{
+  int age = int.Parse(input);
+  WriteLine($"You are {age} years old.");
+}
+catch (OverflowException)
+{
+  WriteLine("Your age is a valid number format but it is either too large or too small.");
+}
+catch (FormatException)
+{
+  WriteLine("The age you entered is not a valid number format");
+}
+catch (Exception ex)
+{
+  WriteLine($"{ex.GetType()} says {ex.Message}");
+}
+WriteLine("After parsing");
+
+Write("Enter an amount: ");
+string? amount = ReadLine();
+try
+{
+  decimal amountValue = decimal.Parse(amount);
+}
+catch (FormatException) when (amount.Contains("$"))
+{
+  WriteLine("Amounts cannnot use the doller sign!");
+}
+catch (FormatException)
+{
+  WriteLine("Amounts must only contain digits!");
+}
