fdb3967dac17493e7b834303b9e00980b1526a5b
yamakenji <yamakenji@gmail.com>
2022-04-08 14:06:47 +0900

    Ch04 : Writing a function that returns a value

--- Chapter04/WritingFunctions/Program.cs
+++ Chapter04/WritingFunctions/Program.cs
@@ -3,7 +3,9 @@
 
 using static System.Console;
 
-TimeTable(10);
+// TimeTable(10);
+decimal taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "FR");
+WriteLine($"You must pay {taxToPay:C} in tax.");
 
 static void TimeTable(byte number)
 {
@@ -13,4 +15,45 @@ static void TimeTable(byte number)
     WriteLine($"{row} x {number} = {row * number}");
   }
   WriteLine();
+}
+
+static decimal CalculateTax(
+  decimal amount, string twoLetterRegionCode)
+{
+  decimal rate = 0.0M;
+  switch (twoLetterRegionCode)
+  {
+    case "CH":
+      rate = 0.08M;
+      break;
+    case "DK":
+    case "NO":
+      rate = 0.25M;
+      break;
+    case "GB":
+    case "FR":
+      rate = 0.2M;
+      break;
+    case "HU":
+      rate = 0.27M;
+      break;
+    case "OR":
+    case "AK":
+    case "MT":
+      rate = 0.0M;
+      break;
+    case "ND":
+    case "WI":
+    case "ME":
+    case "VA":
+      rate = 0.05M;
+      break;
+    case "CA":
+      rate = 0.0825M;
+      break;
+    default:
+      rate = 0.06M;
+      break;
+  }
+  return amount * rate;
 }
