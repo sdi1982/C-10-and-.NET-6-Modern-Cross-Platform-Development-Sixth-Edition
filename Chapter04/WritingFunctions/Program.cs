29598a4dde9de55b62064a4e5dfffad1335b8709
yamakenji <yamakenji@gmail.com>
2022-04-08 11:32:20 +0900

    Add: Chapter04 Times table example Writing a times table function

---
+++ Chapter04/WritingFunctions/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using static System.Console;
+
+TimeTable(10);
+
+static void TimeTable(byte number)
+{
+  WriteLine($"This is the {number} times table:");
+  for (int row = 1; row <= 12; row++)
+  {
+    WriteLine($"{row} x {number} = {row * number}");
+  }
+  WriteLine();
+}
