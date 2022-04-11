382f878a76580029b73deaca9965f80a988084df
yamakenji <yamakenji@gmail.com>
2022-04-11 17:50:31 +0900

    Converting numbers from cardinal to ordinal

--- Chapter04/WritingFunctions/Program.cs
+++ Chapter04/WritingFunctions/Program.cs
@@ -4,8 +4,9 @@
 using static System.Console;
 
 // TimeTable(10);
-decimal taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "FR");
-WriteLine($"You must pay {taxToPay:C} in tax.");
+// decimal taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "FR");
+// WriteLine($"You must pay {taxToPay:C} in tax.");
+RunCardinalToOrdinal();
 
 static void TimeTable(byte number)
 {
@@ -56,4 +57,34 @@ static decimal CalculateTax(
       break;
   }
   return amount * rate;
+}
+
+static void RunCardinalToOrdinal()
+{
+  for (int number = 1; number <= 40; number++)
+  {
+    Write($"{CardinalToOrdinal(number)} ");
+  }
+  WriteLine();
+}
+
+static string CardinalToOrdinal(int number)
+{
+  switch (number)
+  {
+    case 11:
+    case 12:
+    case 13:
+      return $"{number}th";
+    default:
+      int lastDisit = number % 10;
+      string suffix = lastDisit switch
+      {
+        1 => "st",
+        2 => "nd",
+        3 => "rd",
+        _ => "th"
+      };
+      return $"{number}{suffix}";
+  }
 }
