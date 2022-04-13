33572a05c69a8af87dc5a61a51eaefc06c85a717
yamakenji <yamakenji@gmail.com>
2022-04-13 15:53:21 +0900

    Using lambdas in function implementations

--- Chapter04/WritingFunctions/Program.cs
+++ Chapter04/WritingFunctions/Program.cs
@@ -7,7 +7,9 @@ using static System.Console;
 // decimal taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "FR");
 // WriteLine($"You must pay {taxToPay:C} in tax.");
 // RunCardinalToOrdinal();
-RunFactorial();
+// RunFactorial();
+// RunFibImperative();
+RunFibFunctional();
 
 static void TimeTable(byte number)
 {
@@ -127,3 +129,49 @@ static int Factorial(int number)
       return number * Factorial(number - 1);
     }
 }
+
+// Using lambdas in function implementations
+
+static void RunFibImperative()
+{
+  for (int i = 1; i <= 30; i++)
+  {
+    WriteLine("The {0} term of the Fibonacci sequence is {1:N0}.",
+    arg0: CardinalToOrdinal(i),
+    arg1: FibImperative(term: i));
+  }
+}
+
+static int FibImperative(int term)
+{
+  if (term == 1)
+  {
+    return 0;
+  }
+  else if (term == 2)
+  {
+    return 1;
+  }
+  else
+  {
+    return FibImperative(term - 1) + FibImperative(term - 2);
+  }
+}
+
+static void RunFibFunctional()
+{
+  for (int i = 1; i <= 30; i++)
+  {
+    WriteLine("The {0} term of the Fibonacci sequence is {1:N0}.",
+    arg0: CardinalToOrdinal(i),
+    arg1: FibFunctional(term: i));
+  }
+}
+
+static int FibFunctional(int term) =>
+term switch
+{
+  1 => 0,
+  2 => 1,
+  _ => FibFunctional(term - 1) + FibFunctional(term - 2)
+};
