35a8a0e25f16b2c2185e7f6dc03ee8b51be69c2f
Kenji Yamauchi <yamauchi_ke@mti.co.jp>
2022-04-14 18:38:15 +0900

    Throwing and catching exceptions in functions

---
+++ Chapter04/CallStackExceptionHandling/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Console.WriteLine("Hello, World!");
+
+using Packt;
+using static System.Console;
+
+WriteLine("In Main");
+Alpha();
+
+static void Alpha()
+{
+    WriteLine("In Alpha");
+    Beta();
+}
+
+static void Beta()
+{
+    WriteLine("In Beta");
+    try
+    {
+        Calculator.Gamma();
+    }
+    catch (Exception ex)
+    {
+        WriteLine($"Caught this: {ex.Message}");
+        // throw ex;
+        throw;
+        // throw new InvalidOperationException(
+        //     message: "Calculation had invalid values. See inner exception for why.",
+        //     innerException: ex);
+    }
+}
---
+++ Chapter04/CallStackExceptionHandlingLib/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using static System.Console;
+
+// namespace CallStackExceptionHandlingLib;
+namespace Packt;
+
+public class Calculator
+{
+    public static void Gamma()
+    {
+        WriteLine("In Gamma");
+        Delta();
+    }
+
+    private static void Delta()
+    {
+        WriteLine("In Delta");
+        File.OpenText("bad file path");
+    }
+}
