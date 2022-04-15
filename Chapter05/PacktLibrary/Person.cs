8fee409a5b51a19a85c0e48b205d291f6003b0ab
Kenji Yamauchi <yamauchi_ke@mti.co.jp>
2022-04-15 17:53:03 +0900

    Storing data within fields

--- Chapter05/PacktLibrary/Person.cs
+++ Chapter05/PacktLibrary/Person.cs
@@ -5,7 +5,12 @@ namespace Packt.Shared
 {
     public class Person : object
     {
-    }    
+        // fields
+        public string Name;
+        public DateTime DateOfBirth;
+        public WondersOfTheAncientWorld FavoriteAncientWonder;
+        public WondersOfTheAncientWorld BucketList;
+    }
 };
 
 
--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -7,3 +7,32 @@ using static System.Console;
 
 Person bob = new();
 WriteLine(bob.ToString());
+
+bob.Name = "Bob Smith";
+bob.DateOfBirth = new DateTime(1965, 12, 22);
+WriteLine(format: "{0} was born on {1:dddd, d MMMM yyyy}",
+    arg0: bob.Name,
+    arg1: bob.DateOfBirth);
+    
+Person alice = new()
+{
+    Name = "Alice Jones",
+    DateOfBirth = new(1998, 3, 7)
+};
+
+WriteLine(format: "{0} was born on {1:dd MMM yy}",
+    arg0: alice.Name,
+    arg1: alice.DateOfBirth);
+    
+bob.FavoriteAncientWonder = WondersOfTheAncientWorld.StatueOfZeusAtOlympia;
+WriteLine(
+    format: "{0}'s favorite wonder is {1}. Its integer is {2}.",
+    arg0: bob.Name,
+    arg1: bob.FavoriteAncientWonder,
+    arg2: (int)bob.FavoriteAncientWonder);
+
+bob.BucketList = WondersOfTheAncientWorld.HangingGardensOfBabylon
+                 | WondersOfTheAncientWorld.MausoleumAtHalicarnassus;
+
+WriteLine($"{bob.Name}'s bucket list is {bob.BucketList}");
+    
