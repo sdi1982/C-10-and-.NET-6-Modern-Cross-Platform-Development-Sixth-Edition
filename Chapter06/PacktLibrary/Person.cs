bc53568f7213660d8ea2371f0090f0bd30daf860
Kenji Yamauchi <yamauchi_ke@mti.co.jp>
2022-04-27 16:21:14 +0900

    Add: Setting up a class library and console application

---
+++ Chapter06/PacktLibrary/Person.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+namespace Packt.Shared;
+
+public class Person: object
+{
+    public string? Name;
+    public DateTime DateOfBirth;
+    public List<Person> Children = new();
+
+    public void WriteToConsole()
+    {
+        WriteLine($"{Name} was born on a {DateOfBirth:dddd}.");
+    }
+}
