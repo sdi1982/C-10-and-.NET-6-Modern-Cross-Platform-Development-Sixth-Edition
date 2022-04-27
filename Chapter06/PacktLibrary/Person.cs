6f67d351f6494a8048feb8b7c98b2fd8d54e4f3d
Kenji Yamauchi <yamauchi_ke@mti.co.jp>
2022-04-27 17:27:58 +0900

    Add: More about methods

--- Chapter06/PacktLibrary/Person.cs
+++ Chapter06/PacktLibrary/Person.cs
@@ -12,4 +12,42 @@ public class Person: object
     {
         WriteLine($"{Name} was born on a {DateOfBirth:dddd}.");
     }
+
+    public static Person Procreate(Person p1, Person p2)
+    {
+        Person baby = new()
+        {
+            Name = $"Baby of {p1.Name} and {p2.Name}"
+        };
+        p1.Children.Add(baby);
+        p2.Children.Add(baby);
+        return baby;
+    }
+
+    public Person ProcreateWith(Person partner)
+    {
+        return Procreate(this, partner);
+    }
+
+    public static Person operator *(Person p1, Person p2)
+    {
+        return Person.Procreate(p1, p2);
+    }
+
+    public static int Factorial(int number)
+    {
+        if (number < 0)
+        {
+            throw new ArgumentException(
+                $"{nameof(number)} cannot be less than zero.");
+        }
+
+        return localFactorial(number);
+
+        int localFactorial(int localNumber)
+        {
+            if (localNumber < 1) return 1;
+            return localNumber * localFactorial(localNumber - 1);
+        }
+    }
 }
--- Chapter06/PeopleApp/Program.cs
+++ Chapter06/PeopleApp/Program.cs
@@ -1,3 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+using Packt.Shared;
+using static System.Console;
+
+Person harry = new() { Name = "Harry"};
+Person mary = new() { Name = "Mary" };
+Person jill = new() { Name = "Jill" };
+
+Person baby1 = mary.ProcreateWith(harry);
+baby1.Name = "Gary";
+
+Person baby2 = Person.Procreate(harry, jill);
+Person baby3 = harry * mary;
+WriteLine($"{harry.Name} has {harry.Children.Count} children.");
+WriteLine($"{mary.Name} has {mary.Children.Count} children.");
+WriteLine($"{jill.Name} has {jill.Children.Count} children.");
+
+WriteLine(
+    format: "{0}'s first child is named \"{1}\".",
+    arg0: harry.Name,
+    arg1: harry.Children[0].Name);
+
+WriteLine($"5! is {Person.Factorial(5)}");
+    
