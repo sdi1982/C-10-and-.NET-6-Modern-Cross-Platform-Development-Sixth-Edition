018accbbfb99fea3a2bcff9b03ef66406c236d6c
Kenji Yamauchi <yamauchi_ke@mti.co.jp>
2022-05-06 16:46:35 +0900

    Making types safely reusable with generics

--- Chapter06/PeopleApp/Program.cs
+++ Chapter06/PeopleApp/Program.cs
@@ -5,7 +5,7 @@
 using Packt.Shared;
 using static System.Console;
 
-Person harry = new() { Name = "Harry"};
+Person harry = new() { Name = "Harry" };
 Person mary = new() { Name = "Mary" };
 Person jill = new() { Name = "Jill" };
 
@@ -44,3 +44,31 @@ harry.Poke();
 harry.Poke();
 harry.Poke();
 harry.Poke();
+
+// Making types safely reusable with generics
+System.Collections.Hashtable lookupObject = new();
+lookupObject.Add(key: 1, value: "Alpha");
+lookupObject.Add(key: 2, value: "Beta");
+lookupObject.Add(key: 3, value: "Gamma");
+lookupObject.Add(key: harry, value: "Delta");
+
+int key = 2;
+WriteLine(format: "Key {0} has value: {1}",
+    arg0: key,
+    arg1: lookupObject[key]);
+
+WriteLine(format: "Key {0} has value: {1}",
+    arg0: harry,
+    arg1: lookupObject[harry]);
+    
+// generic lookup collection
+Dictionary<int, string> lookupIntString = new();
+lookupIntString.Add(key: 1, value: "Alpha");
+lookupIntString.Add(key: 2, value: "Beta");
+lookupIntString.Add(key: 3, value: "Gamma");
+lookupIntString.Add(key: 4, value: "Delta");
+
+key = 3;
+WriteLine(format: "Key {0} has value: {1}",
+    arg0: key,
+    arg1: lookupIntString[key]);
